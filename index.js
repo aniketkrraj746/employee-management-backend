4077621b6dc919428201ae931295e3b03062ce9f
aniketkrraj746 <aniketkrraj746@gmail.com>
2023-10-13 17:45:03 +0530

    backend file created

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+require("dotenv").config();
+const dbConnect = require("./config/database");
+const userRoutes = require("./routes/user");
+const app = express();
+var cors = require("cors");
+const path = require("path")
+const PORT = process.env.PORT || 4000;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+// Middleware
+app.use(express.json());
+
+app.use("/api/v1", userRoutes);
+
+// static files
+app.use(express.static(path.join(__dirname,"../frontend/build")))
+app.get((req, res) => {
+  res.sendFile(path.join(__dirname,"../frontend/build/index.html"))
+});
+
+// CORS Configuration
+app.listen(PORT, () => {
+  console.log(`THE SERVER IS UP AND RUNNING AT PORT ${PORT}`);
+});
+
+dbConnect();
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Backend is Running and this is '/' Route</h1>`);
+});
